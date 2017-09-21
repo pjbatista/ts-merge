388bcddb406adc71ba54854eeb2855c03bdc2c09
pjbatista <pedrobatista@myself.com>
2017-09-20 23:22:17 -0300

    Adding `FileWorker` to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {CliApplication} from "./cli";
 export {DtsProcessor} from "./dts-processor";
+export {FileWorker} from "./file-worker";
 export {JsProcessor} from "./js-processor";
 export {MergeOptions, MergeContext} from "./utils";
