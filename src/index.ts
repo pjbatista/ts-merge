73386afab6c780613bfc6fe88f2cdf495cbf5a38
pjbatista <pedrobatista@myself.com>
2017-09-20 23:17:39 -0300

    Adding index.ts (fixes types)

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {CliApplication} from "./cli";
+export {DtsProcessor} from "./dts-processor";
+export {JsProcessor} from "./js-processor";
+export {MergeOptions, MergeContext} from "./utils";
